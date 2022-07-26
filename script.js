754c135799a6a5d4b3e6a2fb3e9c69a2537afdf1
nicolasfern-design <90606091+nicolasfern-design@users.noreply.github.com>
2022-07-26 22:40:48 +0200

    a

---
+++ script.js
@@ -0,0 +1,31 @@
+const userCardTemplate = document.querySelector("[data-user-template]")
+const userCardContainer = document.querySelector("[data-user-cards-container]")
+const searchInput = document.querySelector("[data-search]")
+
+let users = []
+
+searchInput.addEventListener("input", e => {
+  const value = e.target.value.toLowerCase()
+  users.forEach(user => {
+    const isVisible =
+      user.name.toLowerCase().includes(value)
+      user.author.toLowerCase().includes(value)
+      user.tag.toLowerCase().includes(value)
+      user.text.toLowerCase().includes(value)
+    user.element.classList.toggle("hide", !isVisible)
+  })
+})
+
+fetch("https://nicolasfern-design.github.io/data.json")
+  .then(res => res.json())
+  .then(data => {
+    users = data.map(user => {
+      const card = userCardTemplate.content.cloneNode(true).children[0]
+      const header = card.querySelector("[data-header]")
+      const body = card.querySelector("[data-body]")
+      header.textContent = user.name
+      body.textContent = user.email, user.author, user.tag, user.text
+      userCardContainer.append(card)
+      return { name: user.name, email: user.email, element: card }
+    })
+  })
